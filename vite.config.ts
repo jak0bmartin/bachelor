12d502388fad9f4864e77ad0622bdeb42fb44a50
Jakob Martin <jakob.mc.martin@gmail.com>
2026-01-06 11:01:20 +0100

    changed paths and cofig files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  base: '/chemie-game/', // Ersetze 'chemie-game' mit deinem Repository-Namen
+  base: '/bachelor/',
   build: {
     outDir: 'dist',
   },
