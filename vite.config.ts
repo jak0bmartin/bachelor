0014aeb50e602a04a996358c24e4f2562f0aafd0
Jakob Martin <jakob.mc.martin@gmail.com>
2026-01-06 10:51:22 +0100

    added and changed config files for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
+  base: '/chemie-game/', // Ersetze 'chemie-game' mit deinem Repository-Namen
   build: {
     outDir: 'dist',
   },
 });
-
-
 
 
 
